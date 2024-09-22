4165d7f2750c68f84db51253f3603efdf7c54d8e
EmmanuelAVT9 <alexsoge@gmail.com>
2024-09-22 12:52:08 -0600

    feat: Add fonts, variables of our color scheme and add fontawesome library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import '@/assets/styles/index.css'
 
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +9,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup>
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
+import { faUser as fasUser } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar} from '@fortawesome/free-regular-svg-icons'
+
+library.add(fasStar,fasUser,farStar)
 </script>
 
 <template>
   <main>
     <h1>Aqui estara la vista Home</h1>
+    Home View <font-awesome-icon icon="fa-solid fa-star" /><font-awesome-icon icon="fa-regular fa-star" />
+    <font-awesome-icon icon="fa-solid fa-user" />
   </main>
 </template>
