752f2b8ea29f31d07508011373631fd3a3d48e54
EmmanuelAVT9 <alexsoge@gmail.com>
2024-09-22 23:16:51 -0600

    feat: Update component pokedex consume pokemonService, functionality button addToTeam and add component PokemonCard consume Pokeapi and Carrucel in modal for evolutions

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,19 +1,60 @@
 <template>
     <div>
-        <p>Soy el componente Pokedex</p>
+        <div class="container-fluid">
+            <div class="row rounded-bottom rounded-5 pt-2 ps-2" style="background-color:  #4A90E2; color:  #FFFFFF;">
+                <div class="col-6 col-md-6 col-sm-12">
+                    <p style="font-size: 2rem; font-weight: bold;"><font-awesome-icon  icon="fa-solid fa-book-open" /> Pokedex</p>
+                </div>
+            </div>
+            <div class="row g-4 mt-2" style="background-color:  #F5F5F4;">
+                <div v-if="loading" class="col-12 text-center">
+                    <p>Cargando Pokémones...</p>
+                </div>
+                <div v-if="error" class="col-12 text-center">
+                    <p>Error al cargar los Pokémones: {{ error }}</p>
+                </div>
+                <div v-for="(pokemon, index) in pokedex" :key="pokemon.name" class="col-md-4 mb-4">
+                    <PokemonCard :pokemon="pokemon" :action="() => addToTeam(pokemon)"
+                        buttonText="Agregar a mi Poketeam" :evolutions="pokemonEvolutions" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    setup () {
-        
+import { getPokemons, getPokemonDetails } from '../api/pokemonService';
+import PokemonCard from './PokemonCard.vue';
 
-        return {}
-    }
-}
+export default {
+    components: {
+        PokemonCard,
+    },
+    data() {
+        return {
+            pokedex: [],
+            loading: false,
+            error: null,
+        };
+    },
+    async created() {
+        this.loading = true;
+        try {
+            const pokemons = await getPokemons();
+            const pokemonDetailsPromises = pokemons.map(pokemon => getPokemonDetails(pokemon.name));
+            this.pokedex = await Promise.all(pokemonDetailsPromises);
+        } catch (error) {
+            this.error = error.message;
+        } finally {
+            this.loading = false;
+        }
+    },
+    methods: {
+        addToTeam(pokemon) {
+            this.$emit('add', pokemon);
+        },
+    },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,153 @@
+<template>
+    <div>
+      <!-- Inicia card del pokémon -->
+      <div class="card text-center" style="background-color: #2A75BB; cursor: pointer;" @click="fetchEvolutions">
+        <h4 class="text-white mt-4">Id del pokémon: {{ pokemon.id }}</h4>
+        <img :src="pokemon.sprites.front_default" class="card-img-top" alt="pokemon image">
+        <div class="card-body d-flex flex-column justify-content-center align-content-center">
+          <hr style="color: #5F97CC;">
+          <p class="card-title text-white fw-bold" style="font-size: 1rem;">Nombre del pokémon:
+            <span style="color: #FFCB05;">{{ pokemon.name }}</span>
+          </p>
+          <button class="btn mt-3" style="background-color: #FFCB05; color: #2A75BB;font-weight: bold;"
+            @click.stop="action">{{ buttonText }}</button>
+        </div>
+      </div>
+      <!-- Termina card del pokémon -->
+  
+      <!-- Modal -->
+      <div class="modal fade" id="pokemonModal" tabindex="-1" aria-labelledby="pokemonModalLabel" aria-hidden="true"
+        :class="{ show: showModal }" :style="{ display: showModal ? 'block' : 'none' }">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="pokemonModalLabel">{{ pokemon.name }}</h5>
+              <button type="button" class="btn-close" style="background-color: #FFCB05; color: #F04037;"
+                @click="toggleModal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <img :src="pokemon.sprites.front_default" class="img-fluid" alt="pokemon image"
+                style="width: 10rem; height: 10rem;">
+              <h4>Habilidades:</h4>
+              <ul>
+                <li v-for="ability in pokemon.abilities" :key="ability.ability.name">{{ ability.ability.name }}</li>
+              </ul>
+              <h4>Evoluciones:</h4>
+              <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item" :class="{ active: index === 0 }" v-for="(evolution, index) in evolutions"
+                    :key="index">
+                    <img :src="evolution.image" class="d-block w-100" alt="evolution image">
+                    <br><br>
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>{{ evolution.name }}</h5>
+                      <p>HP: {{ evolution.hp }}</p>
+                      <h6>Habilidades:</h6>
+                      <p v-for="ability in pokemon.abilities" :key="ability.ability.name">{{ ability.ability.name }}</p>
+                      <br>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+
+  export default {
+    components: {
+    },
+    props: {
+      pokemon: Object,
+      action: Function,
+      buttonText: String
+    },
+    data() {
+      return {
+        showModal: false,
+        evolutions: []
+      };
+    },
+    methods: {
+      toggleModal() {
+        this.showModal = !this.showModal;
+      },
+      async fetchEvolutions() {
+        try {
+          const speciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${this.pokemon.id}/`);
+          const evolutionChainUrl = speciesResponse.data.evolution_chain.url;
+          const evolutionResponse = await axios.get(evolutionChainUrl);
+          this.evolutions = await this.parseEvolutions(evolutionResponse.data.chain);
+          this.toggleModal();
+        } catch (error) {
+          console.error('Error fetching evolutions:', error);
+        }
+      },
+      async parseEvolutions(chain) {
+        const evolutions = [];
+        let current = chain;
+  
+        while (current) {
+          const pokemonId = current.species.url.split('/').slice(-2, -1)[0];
+          const pokemonData = await this.fetchPokemonData(pokemonId);
+  
+          evolutions.push({
+            name: current.species.name,
+            image: pokemonData.sprites.front_default,
+            hp: pokemonData.stats.find(stat => stat.stat.name === 'hp').base_stat,
+            abilities: pokemonData.abilities.map(ability => ability.ability.name)
+          });
+  
+          current = current.evolves_to[0];
+        }
+  
+        return evolutions;
+      },
+      async fetchPokemonData(pokemonId) {
+        try {
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
+          return response.data;
+        } catch (error) {
+          console.error('Error fetching Pokémon data:', error);
+          return null;
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .modal-content {
+    background-color: #2A75BB;
+    color: #FFCB05;
+  }
+  
+  .carousel-caption {
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px;
+  }
+  </style>
+  
