0986edce7b3e176e049502200f33fd8e73afbb48
EmmanuelAVT9 <alexsoge@gmail.com>
2024-09-22 22:59:08 -0600

    feat: Add components Poketeam and Pokedex updated templates, add functionality and modified script sections

--- src/components/Poketeam.vue
+++ src/components/Poketeam.vue
@@ -1,19 +1,40 @@
 <template>
-    <div>
-        <p>Soy el componente Poketeam</p>
+  <div>
+    <div class="row rounded-bottom rounded-5 pt-2 ps-2" style="background-color:  #4A90E2; color:  #FFFFFF;">
+      <p style="font-size: 2rem; font-weight: bold;">
+        <font-awesome-icon icon="fa-solid fa-users" />
+        My Poketeam
+      </p>
     </div>
+    <div class="row g-4 mt-2" style="background-color:  #F5F5F4;">
+      <div class="col-md-4 mb-3" v-for="pokemon in poketeam" :key="pokemon.name">
+        <PokemonCard :pokemon="pokemon" :action="() => removeFromTeam(pokemon)" buttonText="Eliminar" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUsers as fasUsers } from '@fortawesome/free-solid-svg-icons'
+
+import PokemonCard from './PokemonCard.vue';
+
+library.add(fasUsers)
+
 export default {
-    setup () {
-        
-
-        return {}
-    }
-}
+  components: {
+    PokemonCard,
+  },
+  props: {
+    poketeam: Array,
+  },
+  emits: ['remove'],
+  methods: {
+    removeFromTeam(pokemon) {
+      this.$emit('remove', pokemon);
+    },
+  },
+};
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,49 @@
-<script setup>
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
-import { faUser as fasUser } from '@fortawesome/free-solid-svg-icons'
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
-
-import Navbar from '@/components/Navbar.vue';
-import SectionWhite from '@/components/SectionWhite.vue';
-
-library.add(fasStar, fasUser, farStar)
-
-</script>
-
 <template>
   <div style="background-color: #FFCB05;">
     <Navbar />
-    <SectionWhite />
+    <section class="border rounded rounded-5 border-2" style="background-color: #FFFFFF;">
+      <div class="container-fluid mt-5">
+        <div class="row">
+          <div class="col-md-6">
+            <Poketeam :poketeam="poketeam" @remove="removePokemon" />
+          </div>
+          <div class="col-md-6">
+            <Pokedex :pokedex="pokedex" @add="addPokemon" />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import { getPokemons } from '../api/pokemonService';
+import Poketeam from '../components/Poketeam.vue';
+import Pokedex from '../components/Pokedex.vue';
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Poketeam,
+    Pokedex,
+    Navbar,
+  },
+  data() {
+    return {
+      poketeam: [],
+      pokedex: getPokemons(),
+    };
+  },
+  methods: {
+    addPokemon(pokemon) {
+      if (!this.poketeam.includes(pokemon) && this.poketeam.length < 6) {
+        this.poketeam.push(pokemon);
+      }
+    },
+    removePokemon(pokemon) {
+      this.poketeam = this.poketeam.filter((p) => p.name !== pokemon.name);
+    },
+  },
+};
+
+</script>
