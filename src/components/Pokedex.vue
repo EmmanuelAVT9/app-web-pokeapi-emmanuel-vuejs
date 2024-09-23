d29ed02b20a98a7829a5dfd3f3e549cb6935aa83
EmmanuelAVT9 <alexsoge@gmail.com>
2024-09-22 19:52:55 -0600

    feat: Create component for Pokedex

---
+++ src/components/Pokedex.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Soy el componente Pokedex</p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/SectionWhite.vue
+++ src/components/SectionWhite.vue
@@ -1,30 +1,12 @@
 <template>
     <div class="border rounded rounded-5 border-2" style="background-color: #FFFFFF;">
-        <div class="container rounded-5 mt-4">
+        <div class="container rounded-5 mt-5 border">
             <div class="row">
                 <div class="col-xl-6 col-12 col-md-12 col-sm-12">
-                    <div class="container-fluid">
-                        <div class="row rounded-bottom rounded-5 pt-2 ps-2"
-                            style="background-color:  #4A90E2; color:  #FFFFFF;">
-                            <p style="font-size: 2rem; font-weight: bold;">
-                                <font-awesome-icon icon="fa-solid fa-users" />
-                                    Mi Poketeam
-                            </p>
-                        </div>
-                    </div>
+
                 </div>
                 <div class="col-xl-6 col-12 col-md-12 col-sm-12">
-                    <div class="container-fluid">
-                        <div class="row rounded-bottom rounded-5 pt-2 ps-2"
-                            style="background-color:  #4A90E2; color:  #FFFFFF;">
-                            <div class="col-6 col-md-6 col-sm-12">
-                                <p style="font-size: 2rem; font-weight: bold;">
-                                    <font-awesome-icon icon="fa-solid fa-book-open" />
-                                    Pokedex
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <Pokedex />
                 </div>
             </div>
         </div>
@@ -33,16 +15,12 @@
 </template>
 
 <script>
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBookOpen as fasBookOpen } from '@fortawesome/free-solid-svg-icons'
-import { faUsers as fasUsers } from '@fortawesome/free-solid-svg-icons'
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
-
-library.add(fasBookOpen, fasUsers)
+import Pokedex from '../components/Pokedex.vue';
 
 export default {
-
+    components: {
+        Pokedex,
+    },
     setup() {
 
         return {}
